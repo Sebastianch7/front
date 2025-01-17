8d969bb0131e975d1d2c7844fd87dbee05336496
Sebastian Chaparro <sebass7@live.com>
2025-01-17 16:34:04 -0500

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import LocationList from './components/LocationList';
-import { Container, Typography, AppBar, Toolbar, Button, Box } from '@mui/material';
+import {
+  Container,
+  Typography,
+  AppBar,
+  Toolbar,
+  Button,
+  Box,
+} from '@mui/material';
 
 const App: React.FC = () => {
-    return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-            <AppBar position="sticky" sx={{ marginBottom: 2 }}>
-                <Toolbar>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                        Lista de Sedes
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <AppBar position="sticky" sx={{ marginBottom: 2 }}>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Lista de Sedes
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
 
-            <Container>
-                <Typography variant="h4" gutterBottom sx={{ textAlign: 'center' }}>
-                    Explora nuestras Sedes
-                </Typography>
-                <LocationList />
-            </Container>
-        </Box>
-    );
+      <Container>
+        <Typography variant="h4" gutterBottom sx={{ textAlign: 'center' }}>
+          Explora nuestras Sedes
+        </Typography>
+        <LocationList />
+      </Container>
+    </Box>
+  );
 };
 
 export default App;
